3a3432d59bbcd32bb7daa9942cc0df6761ef1ab4
Tim Fisken <contact@timfisken.org>
2017-07-14 14:11:52 +0100

    Add Train application
    Adds an application which trains a model and saves it to disk.

---
+++ analysis/src/main/scala/training/Train.scala
@@ -0,0 +1,36 @@
+package training
+
+import com.intel.imllib.crf.nlp.{CRF, CRFModel}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * A driver application to train a model and save it to a file
+  */
+object Train extends TrainBase {
+  def main(args: Array[String]) = {
+    if (args.length < 1) {
+      println("Supply path to training data")
+      System.exit(1)
+    }
+
+    val trainingPath = args(0)
+
+    if (args.length < 2) {
+      println("Supply a path to write to model to")
+      System.exit(1)
+    }
+
+    val outputPath = args(1)
+
+    val conf = (new SparkConf).setAppName("Train")
+    val sc = new SparkContext(conf)
+
+    val trainingData = loadTrainingData(sc, trainingPath, Seq())
+
+    val templates = FeatureTemplate.templatesAsStrings(Features.templates, unqualifiedBigram=true)
+
+    val model = CRF.train(templates, trainingData)
+
+    CRFModel.saveBinaryFile(model, outputPath)
+  }
+}
