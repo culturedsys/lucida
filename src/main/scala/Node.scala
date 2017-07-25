3849ccc8b361d3dac59494f99ff39e02a4b8a4f6
Tim Fisken <contact@timfisken.org>
2017-07-25 17:11:56 +0100

    Add tree preprocessing functions
    The ZS tree edit distance algorithm requires preprocessing trees
    to get various bits of information, namely:
    
      - the nodes in postorder
      - the left-most descendant of each node
      - the keyroots (i.e., every node with a left sibling).
    
    This commit adds functions to calculate these, along with unit
    tests.

---
+++ src/main/scala/Node.scala
@@ -0,0 +1,71 @@
+/**
+  * A Node, that is, a label and an ordered sequence (possible empty) of child nodes.
+  */
+case class Node[A](label: A, children: Seq[Node[A]]) {
+  /**
+    * Return the nodes in the tree rooted at this node, in tree postorder
+    */
+  def postOrder: Vector[Node[A]] = {
+    def helper(acc: Vector[Node[A]], stack: Seq[Node[A]]): Vector[Node[A]] = stack match {
+      case Seq() => acc
+      case head +: rest =>
+        helper(head +: acc, head.children.reverse ++ rest)
+    }
+
+    helper(Vector(), Seq(this))
+  }
+
+  /**
+    * Return, in postorder, a list of paths from each node to the root.
+    */
+  def postOrderPaths: Vector[Seq[Node[A]]] = {
+    def helper(acc: Vector[Seq[Node[A]]],
+               stack: Seq[Vector[Node[A]]]): Vector[Seq[Node[A]]] =
+      stack match {
+        case Seq() => acc
+        case path +: rest =>
+          val newAcc = path +: acc
+          val newStack = path.head.children.reverse.map(_ +: path) ++ rest
+          helper(newAcc, newStack)
+      }
+    helper(Vector(), Seq(Vector(this)))
+  }
+
+  /**
+    * Return an array, in postorder, of the postorder index of the left-most leaf descendant of
+    * each node in this tree.  That is, `leftMostDescendants(i)` gives the postorder index of the
+    * left-most leaf descendant of the node with postorder index `i`.
+    */
+  def leftMostDescendants: Vector[Int] = leftMostDescendants(postOrderPaths)
+
+  def leftMostDescendants(paths: Vector[Seq[Node[A]]]): Vector[Int] = {
+    def helper(acc: Vector[Int], leafForNode: Map[Node[A], Int],
+               paths: Vector[(Seq[Node[A]], Int)]): Vector[Int] =
+      paths match {
+        case Vector() => acc
+        case (path, index) +: rest =>
+          val (leaf, leafMap) =
+            if (path.head.children.isEmpty) {
+              (index, path.tail.map(_ -> index).toMap ++ leafForNode)
+            } else {
+              (leafForNode(path.head), leafForNode)
+            }
+          helper(acc :+ leaf, leafMap, rest)
+      }
+    helper(Vector(), Map(), paths.zipWithIndex)
+  }
+
+  /**
+    * Return the postorder indexes of the keyroots of the tree, in increasing order. Keyroots are
+    * those nodes where there is not node with a later postorder index which has the same
+    * left-most descendant; this is equivalent to the nodes which have left siblings.
+    */
+  def keyroots: Seq[Int] = keyroots(leftMostDescendants)
+
+  def keyroots(lmds: Seq[Int]): Seq[Int] = {
+    val roots = lmds.zipWithIndex.foldLeft(Map[Int, Int]()) { (roots, lmdAndIndex) =>
+      roots + lmdAndIndex
+    }
+    roots.values.toSeq.sorted
+  }
+}
---
+++ src/test/scala/NodeSpec.scala
@@ -0,0 +1,120 @@
+import org.scalatest.FunSpec
+
+/**
+  * Tests for general utilities related to manipulating and traversing Nodes
+  */
+class NodeSpec extends FunSpec {
+  describe("A node") {
+    describe("postOrder") {
+      it("should return just the root for a tree with no children") {
+        val tree = Node("1", Seq())
+        assert(tree.postOrder == Vector(tree))
+      }
+
+      it("should return children before parents for a two-level tree") {
+        val b = Node("b", Seq())
+        val c = Node("c", Seq())
+        val a = Node("a", Seq(b, c))
+        assert(a.postOrder.map(_.label) == Vector("b", "c", "a"))
+      }
+
+      it("should return post-order for a multi-level tree") {
+        val d = Node("d", Seq())
+        val e = Node("e", Seq())
+        val f = Node("f", Seq())
+        val g = Node("g", Seq())
+        val b = Node("b", Seq(d, e))
+        val c = Node("c", Seq(f, g))
+        val a = Node("a", Seq(b, c))
+
+        assert(a.postOrder.map(_.label) == Vector("d", "e", "b", "f", "g", "c", "a"))
+      }
+    }
+
+    describe("postOrderPaths") {
+      it("should return a single element path for a single-element tree") {
+        val tree = Node("a", Seq())
+        assert(tree.postOrderPaths == Vector(Seq(tree)))
+      }
+
+      it("should return a one-element path for child nodes in a two-level tree") {
+        val b = Node("b", Seq())
+        val c = Node("c", Seq())
+        val a = Node("a", Seq(b, c))
+
+        assert(a.postOrderPaths.map(_.tail) == Vector(Seq(a), Seq(a), Seq()))
+      }
+
+      it("should return two-element paths for child nodes in a three-level tree") {
+        val c = Node("c", Seq())
+        val b = Node("b", Seq(c))
+        val a = Node("a", Seq(b))
+
+        assert(a.postOrderPaths.map(_.map(_.label)) ==
+          Vector(Seq("c", "b", "a"), Seq("b", "a"), Seq("a")))
+      }
+    }
+
+    describe("leftMostDescendants") {
+      it("should return the root for a one-element tree") {
+        val tree = Node("a", Seq())
+
+        assert(tree.leftMostDescendants == Vector(0))
+      }
+
+      it("should return the left-most node for a two-level tree") {
+        val b = Node("b", Seq())
+        val c = Node("c", Seq())
+        val a = Node("a", Seq(b, c))
+
+        assert(a.leftMostDescendants == Vector(0, 1, 0))
+      }
+
+      it("should return the left-most node for a three-level tree") {
+        val c = Node("c", Seq())
+        val b = Node("b", Seq(c))
+        val a = Node("a", Seq(b))
+
+        assert(a.leftMostDescendants == Vector(0, 0, 0))
+      }
+    }
+
+    describe("keyroots") {
+      it("should return the root for a one-node tree") {
+        val tree = Node("a", Seq())
+
+        assert(tree.keyroots == Vector(0))
+      }
+
+      it("should return the root and the right siblings for a two-level tree") {
+        val b = Node("b", Seq())
+        val c = Node("c", Seq())
+        val a = Node("a", Seq(b, c))
+
+        assert(a.keyroots == Seq(1, 2))
+      }
+
+      it("should return all the right siblings in a two-level tree") {
+        val b = Node("b", Seq())
+        val c = Node("c", Seq())
+        val d = Node("d", Seq())
+        val a = Node("a", Seq(b, c, d))
+
+        assert(a.keyroots == Seq(1, 2, 3))
+      }
+
+      it("should return all the right siblings in a three-level tree") {
+        val tree = Node("a", Seq(
+          Node("b", Seq(
+            Node("e", Seq()),
+            Node("f", Seq())
+          )),
+          Node("c", Seq(Node("g", Seq()))),
+          Node("d", Seq())
+        ))
+
+        assert(tree.keyroots == Seq(1, 4, 5, 6))
+      }
+    }
+  }
+}
